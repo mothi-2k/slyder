69cc6858a3fb00d869a7d192934281b2f48b7095
Mothish <mothishdeenadayalan@gmail.com>
2022-10-30 21:53:04 +0530

    changed from grid to css-transform based cell movement transition is not supported for grid-area

--- main.js
+++ main.js
@@ -1,47 +1,31 @@
 grid = []
+
 for (let i = 0; i < 4; i++) {
     row = [];
     for (let j = 0; j < 4; j++) {
         if (i === 3 && j == 3) {
             row[j] = 0
-            continue;
-        }
-        row[j] = 1;
-    }
-    grid[i] = row;
-} 
-
-for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-        if (i === 3 && j == 3) {
             continue
         }
+        row[j] = 1;
         let cell = document.createElement('div')
         cell.setAttribute('row', i);
         cell.setAttribute('col', j);
-        cell.addEventListener("click", (event) => checkGap(cell));
-        cell.style.transition = 'all 5s';
+        cell.addEventListener("click", (event) => move(cell));
+        cell.style.transform = `translate(${j * 99}%, ${i * 99}%)`;
         cell.style.backgroundPosition = `-${(j)*20}vw -${(i)*20}vw`
-        cell.style.gridRowStart = i + 1;
-        cell.style.gridRowEnd = i + 2;
-        cell.style.gridColumnStart = j + 1;
-        cell.style.gridColumnEnd = j + 1;
         cell.classList = `cell cell${i}${j}`
         mainBoard = document.getElementsByClassName('main-board')[0]
         mainBoard.appendChild(cell);
     }
+    grid[i] = row;
 } 
 
 
-
-
-// checkGap(grid)
-
-
-function checkGap(cell) {
+function move(cell) {
     row = parseInt(cell.getAttribute('row'));
     col = parseInt(cell.getAttribute('col'));
-    console.log(row, col);
+    // console.log(row, col);
     let result = -1
 
     if (row - 1 >= 0 && !grid[row - 1][col]) {
@@ -59,13 +43,35 @@ function checkGap(cell) {
     if (result !== -1) {
         grid[row][col] = 0;
         grid[result[0]][result[1]] = 1;
-        cell.style.gridRowStart = result[0] + 1;
-        cell.style.gridRowEnd = result[0] + 2;
-        cell.style.gridColumnStart = result[1] + 1;
-        cell.style.gridColumnEnd = result[1] + 2;
+        cell.style.transform = `translate(${result[1] * 99}%, ${result[0] * 99}%)`;
         cell.setAttribute('row', result[0]);
         cell.setAttribute('col', result[1]);
-        console.log('result' + result)
     }
+    isSolved();
     return result;
 }
+
+function isSolved() {
+    let solved = true;
+    let mainBoard = document.getElementsByClassName('main-board')[0];
+    for (let cell = 0; cell < mainBoard.children.length; cell = cell + 1) {
+        let piece = mainBoard.children.item(cell);
+        solved = solved && checkCellPos(piece);
+    }
+    if (solved) {
+        console.log('yes solved');
+    }
+    
+}
+
+function checkCellPos(piece) {
+    let currentRow = parseInt(piece.getAttribute('row'));
+    let currentCol = parseInt(piece.getAttribute('col'));
+    let className = piece.classList[1];
+    let validRow = parseInt(className.substring(className.length-2, className.length-1));
+    let validCol = parseInt(className.substring(className.length-1));
+    if (currentRow === validRow && currentCol === validCol) {
+        return true;
+    }
+}
+isSolved();
